dfc1bf2efdb3ffc809d6fc0ce6037eedf419f1b3
Alumno <Alumno@LAB701-11>
2018-02-26 10:38:32 -0500

    multi y division

--- src/main/java/Objeto.java
+++ src/main/java/Objeto.java
@@ -1,7 +1,30 @@
+import java.util.Scanner;
+
+import lab3.revata.operaciones.Operacion3;
 
 public class Objeto {
 
 	
-	public String aaa;
+	
+	
+	private static Scanner s;
+
+	public static void main(String[] args) {
+		int opcion;
+		s = new Scanner(System.in);
+		System.out.println("Primer numero");
+		Operacion3.a = s.nextDouble();
+		System.out.println("Segundo numero");
+		Operacion3.b = s.nextDouble();
+		System.out.println("Operacion:\n1. Multiplicacion\n2. Division");
+		opcion = s.nextInt();
+		
+		if(opcion==1)
+			System.out.println("Resultado: " + Operacion3.multiplicacion());
+		else if(opcion==2)
+			System.out.println("Resultado: " + Operacion3.division());
+		else
+			System.out.println("No fokiu");
+	}
 	
 }
---
+++ src/main/java/lab3/revata/operaciones/Operacion3.java
@@ -0,0 +1,17 @@
+package lab3.revata.operaciones;
+
+public class Operacion3 {
+	public static double a;
+	public static double b;
+	
+	
+	
+	public static double multiplicacion() {
+		return a * b;
+	}
+	
+	public static double division() {
+		return a / b;
+	}
+	
+}
