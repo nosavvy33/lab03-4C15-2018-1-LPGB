83022f4899d5511aeff1680ee64dbc363e04fddf
nosavvy33 <bleonp1997@gmail.com>
2018-02-26 11:29:25 -0500

    changed

--- src/main/java/com/tecsup/lab2/git/controller/StudentController.java
+++ src/main/java/com/tecsup/lab2/git/controller/StudentController.java
@@ -1,15 +1,7 @@
-<<<<<<< HEAD
+
 package com.tecsup.lab2.git.controller;
 
 public class StudentController {
-
-	
-	public String attr = "This is a message, hopefully to be seen in GitHub.";
-}
-=======
-package com.tecsup.lab2.git.controller;
-
-public class StudentController {
+	public String attr = "This is a message, hopefully to be seen in GitHub...";
 
 }
->>>>>>> branch 'sprint-2' of https://github.com/nosavvy33/lab03-4C15-2018-1-LPGB
